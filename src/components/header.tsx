e62ef6ce470f02db7428669397e7dc13a9f72b44
Cyril Clement <dossiman@gmail.com>
2020-06-21 23:48:11 +0700

    add stripe payment as a lambda function

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'gatsby';
 import React, { FC, useEffect, useState } from 'react';
+import StripeCheckout, { Token } from 'react-stripe-checkout';
 import './header.css';
 
 const Header: FC = () => {
@@ -20,6 +21,23 @@ const Header: FC = () => {
     }
   };
 
+  const handlePurchase = (token: Token) => {
+    const amount = 5000;
+    const description = 'My awesome product';
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.card.name,
+      description,
+      amount,
+    };
+
+    fetch('http://localhost:9000/stripe-charge', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject),
+    });
+  };
+
   return (
     <header className={hasScrolled ? 'header header-scrolled' : 'header'}>
       <div className="header-group">
@@ -33,9 +51,13 @@ const Header: FC = () => {
         <Link to="/courses">Courses</Link>
         <Link to="/downloads">Downloads</Link>
         <Link to="/workshops">Workshops</Link>
-        <Link to="/buy">
+        <StripeCheckout
+          amount={5000}
+          stripeKey="pk_test_WcFTC3kSlmucjD7qmQk61zem00wxfE4R3M"
+          token={handlePurchase}
+        >
           <button>Buy</button>
-        </Link>
+        </StripeCheckout>
       </div>
     </header>
   );
